9b3005ba7fe78496802004e08c84cf56b005037e
Erefeu <lipe.person@gmail.com>
2026-05-05 16:09:28 -0300

    Update bluetooth.js

--- js/bluetooth.js
+++ js/bluetooth.js
@@ -1,5 +1,5 @@
 // ═══════════════════════════════════════════════
-//  SMART CUBE BLUETOOTH — QiYi Tornado V3
+//  SMART CUBE BLUETOOTH — QiYi Tornado V4
 //  Protocolo baseado no cstimer (parseQYData)
 //  Ref: github.com/cs-bin/cstimer
 // ═══════════════════════════════════════════════
